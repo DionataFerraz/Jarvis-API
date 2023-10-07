c0ba2871d7aa382a0c2daa19a0e0115c802df561
ViniLemess <viniciuslemescaetano@gmail.com>
2023-10-07 16:16:57 -0300

    Commit as coisas que faltaram

---
+++ src/main/kotlin/br/com/jarvis/config/security/SecurityConfiguration.kt
@@ -0,0 +1,53 @@
+package br.com.jarvis.config.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfiguration {
+
+    @Bean
+    fun securityFilterChain(httpSecurity: HttpSecurity, securityFilter: SecurityFilter): SecurityFilterChain {
+        return httpSecurity.csrf {
+            it.disable()
+        }.sessionManagement {
+            it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        }.authorizeHttpRequests {
+            it.requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+            it.requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+            it.requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+        }.build()
+    }
+
+    @Bean
+    fun authenticationManager(authenticationConfiguration: AuthenticationConfiguration): AuthenticationManager {
+        return authenticationConfiguration.authenticationManager
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    fun webSecurityCustomizer(): WebSecurityCustomizer {
+        return WebSecurityCustomizer { web: WebSecurity ->
+            web.ignoring().requestMatchers(
+                "/v3/api-docs",
+                "/v3/api-docs/**",
+                "/swagger-resources/**",
+                "/swagger-ui/**"
+            )
+        }
+    }
+}
